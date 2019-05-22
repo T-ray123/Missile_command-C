8cbbace039b16f907351c40f537e9221418aa5bc
tomamad <tomamadanski@gmail.com>
2019-05-22 02:05:45 +0100

    Added window and a menu for choices

--- main_menu.c
+++ main_menu.c
@@ -8,6 +8,7 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 #define CTRLD 4
 
+//adding options for menu
 char *choices[] = 
 {
     "Play",
@@ -15,3 +16,38 @@ char *choices[] =
     "Exit",
     (char *)NULL,
 };
+
+int main menu()
+{
+    ITEM** my_items;
+    MENU* menu;
+    WINDOW* mwin; //move window
+
+    keypad(stdscr, TRUE);
+
+    int n_choices = ARRAY_SIZE(choices); //making an array of the choices
+
+    my_items = (ITEM **) calloc(n_choices, sizeof(ITEM *));
+   
+    for(int i = 0; i < n_choices; ++i)
+        my_items[i] = new_item(choices[i], "");
+
+    /**
+     * TO DO
+     */
+    int width = COLS / 3;
+    int height = LINES - LINES / 3;
+	menu = new_menu((ITEM **) my_items);
+    mwin = newwin(0, 0, 0, 0); //spawning a new window
+    keypad(mwin, TRUE);
+
+    set_menu_win(menu, mwin);
+    set_menu_sub(menu, derwin(mwin, 6, 8, LINES / 2, COLS / 2));
+    set_menu_mark(menu, " * ");
+
+	refresh();
+
+	post_menu(menu);
+	wrefresh(mwin);
+}
+
