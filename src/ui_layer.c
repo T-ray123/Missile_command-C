8ed5357a12f6a61fd32fe755cd2f7406ec6b3455
tomamad <tomamadanski@gmail.com>
2019-05-22 02:59:22 +0100

    Added functions for updating/init the screen

--- src/ui_layer.c
+++ src/ui_layer.c
@@ -21,9 +21,22 @@ static void update_buildings();
 static void update_base();
 static void reset_buildings();
 
+/**
+ * adding function for initialising the display
+ */
+void init_display() 
 
-void init_display() {
+{
     DISPLAY = newwin(0, 0, 0, 0);
     init_pair(5, COLOR_CYAN, COLOR_BLACK);
-    wattron(DISPLAY, COLOR_PAIR(5));
+    wattron(DISPLAY, COLOR_PAIR(5)); //The routine attron turns on the named attributes without affecting any others.
+}
+
+//10 because each base has 10 missiles in total 
+void update_display() 
+{
+    update_buildings();
+    update_score();
+    update_base();
+    mvwprintw(DISPLAY, 1, COLS - 10, "Round %d ", round);
 }
