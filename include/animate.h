7950c3e805ebb02c6e7db7bc8a9b17eceb7713a7
tomamad <tomamadanski@gmail.com>
2019-05-22 09:36:29 +0100

    Adding header for animate layer

---
+++ include/animate.h
@@ -0,0 +1,23 @@
+#ifndef ANIMATE_H
+#define ANIMATE_H
+
+#include <ncurses.h>
+#include "canvas.h"
+
+#define SECOND 1000000L
+
+/**
+ * function pointers pointing to the different functions used in the program
+ */
+void lerp(Vector*);
+void set_animation(Sprite*, Coord*, Coord*, int);
+void update_animation(Canvas*, Sprite*);
+double distance(Coord*, Coord*);
+double slope(Coord*, Coord*);
+bool cmpcoord(Coord*, Coord*);
+bool is_animation_done(Sprite*);
+unsigned long get_time();
+
+
+
+#endif
