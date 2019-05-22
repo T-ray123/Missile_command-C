a7410e9ecf4395b3ad296bf0633e82dc4a3a8c6a
tomamad <tomamadanski@gmail.com>
2019-05-22 03:05:03 +0100

    Added functions for updating the score and the bases

--- src/ui_layer.c
+++ src/ui_layer.c
@@ -40,3 +40,44 @@ void update_display()
     update_base();
     mvwprintw(DISPLAY, 1, COLS - 10, "Round %d ", round);
 }
+static void update_score() {
+    mvwprintw(DISPLAY, 1, COLS / 2, "Score: %d ", score);
+}
+
+//structuring the base
+static void update_base()
+ {
+    for (int i = 0; i < 3; i++) 
+    {
+        struct Base* base = bases[i];
+
+        draw_building(&base->position, 5);
+        mvwprintw(
+            DISPLAY,
+            base->position.y - 4,  //the difference in terms of pixels toward each building
+            base->position.x - 6,
+            "%d ", base->missile_count
+        );
+    }
+}
+
+/*
+ * Only drawing the buildings on the map
+*/
+static void update_buildings() 
+{
+    int xincrement = COLS * .8 / 10;
+    Coord pos = { .x = xincrement * 2, .y = LINES };
+    for (int i = 0; i < 10; i++) 
+    {
+        if (i < building_count) 
+        {
+            draw_building(&pos, 1); //by one pixel
+        } 
+        else 
+        {
+            clear_building(&pos, 1);
+        }
+        pos.x += xincrement;
+    }
+}
